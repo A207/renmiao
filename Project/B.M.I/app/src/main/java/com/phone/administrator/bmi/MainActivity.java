2b5c781437bad968c1a4f342cb0d4ea6219aa616
renmiao <1017667643@qq.com>
2015-07-14 15:52:41 +0800

    project

--- Project/B.M.I/app/src/main/java/com/phone/administrator/bmi/MainActivity.java
+++ Project/B.M.I/app/src/main/java/com/phone/administrator/bmi/MainActivity.java
@@ -1,6 +1,7 @@
 package com.phone.administrator.bmi;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,15 +16,33 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.btn);
         button.setOnClickListener(calcBMI);
+        Button bt=(Button)findViewById(R.id.help);
+        bt.setOnClickListener(aboutBMI);
+       /* bt.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view){
+
+            }
+        });*/
 
 
     }
+    private OnClickListener aboutBMI=new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent=new Intent(MainActivity.this,OtherActicity.class);
+            startActivity(intent);
+
+        }
+    };
 
     private OnClickListener calcBMI = new OnClickListener() {
         public void onClick(View view) {
