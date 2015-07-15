67ece25194d6608224bfcd1071511bc6cd4da3b9
renmiao <1017667643@qq.com>
2015-07-15 17:40:50 +0800

    project

--- Project/B.M.I/app/src/main/java/com/phone/administrator/bmi/MainActivity.java
+++ Project/B.M.I/app/src/main/java/com/phone/administrator/bmi/MainActivity.java
@@ -3,11 +3,14 @@ package com.phone.administrator.bmi;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
 import java.text.DecimalFormat;
@@ -18,14 +21,19 @@ public class MainActivity extends Activity {
 
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.btn);
         button.setOnClickListener(calcBMI);
         Button bt=(Button)findViewById(R.id.help);
         bt.setOnClickListener(aboutBMI);
+
+
+
        /* bt.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View view){
@@ -35,6 +43,7 @@ public class MainActivity extends Activity {
 
 
     }
+
     private OnClickListener aboutBMI=new OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -46,6 +55,8 @@ public class MainActivity extends Activity {
 
     private OnClickListener calcBMI = new OnClickListener() {
         public void onClick(View view) {
+
+
             DecimalFormat nf = new DecimalFormat("0.00");
             EditText fieldheight = (EditText) findViewById(R.id.shengao2);
             EditText fieldweight = (EditText) findViewById(R.id.tizhong2);
@@ -77,6 +88,7 @@ public class MainActivity extends Activity {
             } else {
                 fieldsuggest.setText(R.string.weight_fat4);
             }
+
 
         }
     };
---
+++ Project/B.M.I/app/src/main/java/com/phone/administrator/bmi/SplashActivity.java
@@ -0,0 +1,48 @@
+package com.phone.administrator.bmi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Message;
+
+import android.os.Handler;
+import android.view.Window;
+
+import java.util.logging.LogRecord;
+
+
+/**
+ * Created by Administrator on 2015/7/14.
+ */
+public class SplashActivity extends Activity {
+    private final int SPLASH_DISPLAY_LENGHT = 2000; //延迟三秒
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        new Handler().postDelayed(new Runnable(){
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        }, SPLASH_DISPLAY_LENGHT);
+    }
+    /*protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        });
+
+        };*/
+
+    }
